6ba000d34f7e3d926c087ce532a4e514e02a1443
marjo <mballabani@helius-software.com>
2017-04-05 13:15:11 +0200

    fix db uri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://marjoballabani:00692485814om3ga@ds147965.mlab.com:47965/smartbar');
+mongoose.connect('mongodb://marjo:om3gachg@ds147965.mlab.com:47965/smartbar');
 // mongoose.connect('mongodb://localhost/smartbar');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'db connection error'));
